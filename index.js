84dae1b12f268156ba291a31ae7c1dc1b076f787
sorasora313 <sky.313.sora@gmail.com>
2022-09-27 07:23:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,20 @@ window.addEventListener('load', function(){
   const pullDownParents = this.document.getElementById("pull-down")
 
   pullDownButton.addEventListener('mouseover', function(){
-    pullDownButton.setAttribute("style", "background-color:#FFBEDA;")
+    this.setAttribute("style", "background-color:#FFBEDA;")
   })
 
   pullDownButton.addEventListener('mouseout', function(){
-    pullDownButton.removeAttribute("style", "background-color:#FFBEDA;")
+    this.removeAttribute("style", "background-color:#FFBEDA;")
   })
 
   pullDownButton.addEventListener('click', function() {
-    pullDownParents.setAttribute("style", "display:block;")
+    if (pullDownParents.getAttribute("style") == "display:block;") {
+      // pullDownParentsにdisplay:block;が付与されている場合（つまり表示されている時）実行される
+      pullDownParents.removeAttribute("style", "display:block;")
+    } else {
+      // pullDownParentsにdisplay:block;が付与されていない場合（つまり非表示の時）実行される
+      pullDownParents.setAttribute("style", "display:block;")
+    }
   })
 })
