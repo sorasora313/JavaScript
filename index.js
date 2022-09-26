d976ef19934b99db315947daf3d439977be96918
sorasora313 <sky.313.sora@gmail.com>
2022-09-26 17:34:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 window.addEventListener('load', function(){
+
   const pullDownButton = document.getElementById("lists")
 
   pullDownButton.addEventListener('mouseover', function(){
-    console.log("mouseover OK")
+    pullDownButton.setAttribute("style", "background-color:#FFBEDA;")
   })
 
-  pullDownButton.addEventListener('mouseout',function(){
+  pullDownButton.addEventListener('mouseout', function(){
     console.log("mouseout OK")
   })
 
- pullDownButton.addEventListener('click', function() {
+  pullDownButton.addEventListener('click', function() {
     console.log("click OK")
- })
+  })
 })
