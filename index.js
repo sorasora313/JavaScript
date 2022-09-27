ba9eeacd7e2be368484b5487951e05ae3e8ec4fb
sorasora313 <sky.313.sora@gmail.com>
2022-09-28 08:16:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -12,12 +12,16 @@ window.addEventListener('load', function(){
   })
 
   pullDownButton.addEventListener('click', function() {
+  
     if (pullDownParents.getAttribute("style") == "display:block;") {
-      // pullDownParentsにdisplay:block;が付与されている場合（つまり表示されている時）実行される
       pullDownParents.removeAttribute("style", "display:block;")
     } else {
-      // pullDownParentsにdisplay:block;が付与されていない場合（つまり非表示の時）実行される
       pullDownParents.setAttribute("style", "display:block;")
     }
+  })
+  pullDownChild.forEach(function(list) {
+    list.addEventListener('click', function(){
+      console.log(list)
+    })
   })
 })
