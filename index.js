11bfc6e26ab98f08eda31d7be61d4f061fe47177
sorasora313 <sky.313.sora@gmail.com>
2022-09-28 08:39:36 +0900

    9/28

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-window.addEventListener('load', function(){
+function pullDown() {
 
   const pullDownButton = document.getElementById("lists")
-  const pullDownParents = this.document.getElementById("pull-down")
+  const pullDownParents = document.getElementById("pull-down")
+  const pullDownChild = document.querySelectorAll(".pull-down-list")
+  const currentList = document.getElementById("current-list")
 
   pullDownButton.addEventListener('mouseover', function(){
     this.setAttribute("style", "background-color:#FFBEDA;")
@@ -12,16 +14,19 @@ window.addEventListener('load', function(){
   })
 
   pullDownButton.addEventListener('click', function() {
-  
     if (pullDownParents.getAttribute("style") == "display:block;") {
       pullDownParents.removeAttribute("style", "display:block;")
     } else {
       pullDownParents.setAttribute("style", "display:block;")
     }
   })
+
   pullDownChild.forEach(function(list) {
-    list.addEventListener('click', function(){
-      console.log(list)
+    list.addEventListener('click', function() {
+      const value = list.innerHTML
+      currentList.innerHTML = value
     })
   })
-})
+}
+
+window.addEventListener('load', pullDown)
